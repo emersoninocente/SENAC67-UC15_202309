8335171c367217251957df146aa9c58f6dba6187
Emerson Meneses Inocente <emerson_inocente@hotmail.com>
2023-11-13 20:25:40 -0300

    Aula 21

---
+++ aula-21/material_aula/functions.js
@@ -0,0 +1,10 @@
+function sorvete(sabor){
+    if(sabor === "chocolate"){
+        alert("Meu sabor favorito!");
+    }
+}
+
+multiplica(arg1,arg2){
+    let resultado = arg1 * arg2;
+    return resultado;
+}
